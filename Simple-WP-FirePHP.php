5e6b602b64c4cec272aa8099df338b7fd9b1eab5
cori schlegel <cori.schlegel@gmail.com>
2010-04-07 19:43:09 -0500

    initial commit

---
+++ Simple-WP-FirePHP.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Simple WP FirePHP
+Plugin URI: http://kinrowan.net/wordpress/Simple-WP-FirePHP
+Description: Simplify Including FIrePHP extensions in a Wordpress BLog for inline debugging
+Version: 0.1.0
+Author: Cori Schlegel
+Author URI: http://kinrowan.net
+*/
+/*
+
+Changelog:
+v.0.1: Alpha
+
+*/
+/*
+Credits: 
+	This template is based on the template at http://pressography.com/plugins/wordpress-plugin-template/ 
+	My changes are documented at http://soderlind.no/archives/2010/03/04/wordpress-plugin-template/
+*/
+
+include_once( "FirePHPCore/FirePHP.class.php");
+include_once( "FirePHPCore/fb.php");
+
+?>
